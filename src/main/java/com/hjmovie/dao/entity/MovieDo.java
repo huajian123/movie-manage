0608334cf52d235c686cb818a50954aaed83917f
huajian <287643967@qq.com>
2020-02-11 17:37:47 +0800

    优化代码

--- src/main/java/com/hjmovie/dao/entity/MovieDo.java
+++ src/main/java/com/hjmovie/dao/entity/MovieDo.java
@@ -1,7 +1,9 @@
 package com.hjmovie.dao.entity;
 
-import java.util.Date;
+import lombok.Data;
 
+import java.util.Date;
+@Data
 public class MovieDo {
     private int id;
     private Date releaseTime;
@@ -12,76 +14,4 @@ public class MovieDo {
     private int votes;
     private String cover;
     private String movieName;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public Date getReleaseTime() {
-        return releaseTime;
-    }
-
-    public void setReleaseTime(Date releaseTime) {
-        this.releaseTime = releaseTime;
-    }
-
-    public int getRemainVotes() {
-        return remainVotes;
-    }
-
-    public void setRemainVotes(int remainVotes) {
-        this.remainVotes = remainVotes;
-    }
-
-    public int getBoxOffice() {
-        return boxOffice;
-    }
-
-    public void setBoxOffice(int boxOffice) {
-        this.boxOffice = boxOffice;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    public double getScore() {
-        return score;
-    }
-
-    public void setScore(double score) {
-        this.score = score;
-    }
-
-    public int getVotes() {
-        return votes;
-    }
-
-    public void setVotes(int votes) {
-        this.votes = votes;
-    }
-
-    public String getCover() {
-        return cover;
-    }
-
-    public void setCover(String cover) {
-        this.cover = cover;
-    }
-
-    public String getMovieName() {
-        return movieName;
-    }
-
-    public void setMovieName(String movieName) {
-        this.movieName = movieName;
-    }
 }
--- src/main/java/com/hjmovie/dao/entity/UserDo.java
+++ src/main/java/com/hjmovie/dao/entity/UserDo.java
@@ -1,5 +1,8 @@
 package com.hjmovie.dao.entity;
 
+import lombok.Data;
+
+@Data
 public class UserDo {
     private int id;
     private String name;
@@ -7,43 +10,5 @@ public class UserDo {
     private String buyMovie;
     private double balance;
 
-    public int getId() {
-        return id;
-    }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getBuyMovie() {
-        return buyMovie;
-    }
-
-    public void setBuyMovie(String buyMovie) {
-        this.buyMovie = buyMovie;
-    }
-
-    public double getBalance() {
-        return balance;
-    }
-
-    public void setBalance(double balance) {
-        this.balance = balance;
-    }
 }
